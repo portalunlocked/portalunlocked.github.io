a8db645b1102054c44b0483ab3646af7ea87cf69
ahnaft <ahnaft2019+1@gmail.com>
2023-03-18 18:38:16 -0500

    updated PWA

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
+const images = ['fox1', 'fox2', 'fox3', 'fox4'];
+const imgElem = document.querySelector('img');
+
+function randomValueFromArray(array) {
+  const randomNo = Math.floor(Math.random() * array.length);
+  return array[randomNo];
+}
+
+setInterval(() => {
+  const randomChoice = randomValueFromArray(images);
+  imgElem.src = `images/${randomChoice}.jpg`;
+}, 2000);
+
+// Register service worker to control making site work offline
+
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker
+    .register('/pwa-examples/a2hs/sw.js')
+    .then(() => { console.log('Service Worker Registered'); });
+}
+
+// Code to handle install prompt on desktop
+
 let deferredPrompt;
 const addBtn = document.querySelector('.add-button');
 addBtn.style.display = 'none';
