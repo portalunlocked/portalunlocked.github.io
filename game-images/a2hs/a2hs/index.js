f83ca95d647410e7f2fababe406e5c5fe000c544
ahnaft <ahnaft2019+1@gmail.com>
2023-03-19 12:54:17 -0500

    Test #5

--- game-images/a2hs/a2hs/index.js
+++ game-images/a2hs/a2hs/index.js
@@ -1,15 +1,4 @@
-const images = ['fox1', 'fox2', 'fox3', 'fox4'];
-const imgElem = document.querySelector('img');
 
-function randomValueFromArray(array) {
-  const randomNo = Math.floor(Math.random() * array.length);
-  return array[randomNo];
-}
-
-setInterval(() => {
-  const randomChoice = randomValueFromArray(images);
-  imgElem.src = `images/${randomChoice}.jpg`;
-}, 2000);
 
 // Register service worker to control making site work offline
 
